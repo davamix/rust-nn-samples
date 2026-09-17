6a5b6d407787faa6d1efc99a01c455667b7e672e
Daniel Valcarce <davamix@gmail.com>
2026-09-17 18:02:36 +0200

    nn-from-scratch: Added layer 3 weight. Fully forward

--- nn-from-scratch/src/main.rs
+++ nn-from-scratch/src/main.rs
@@ -2,17 +2,30 @@ use std::f64::consts::E;
 
 fn main() {
     // layers: input -> hidden -> output. 3 nodes each
+
+    // Layer 1: input
     let (i1, i2, i3) = (0.9, 0.1, 0.8);
-    let (w11, w21, w31) = (0.9, 0.3, 0.4);
-    let (w12, w22, w32) = (0.2, 0.8, 0.2);
-    let (w13, w23, w33) = (0.1, 0.5, 0.6);
+
+    // Layer 2: hidden weights
+    let (hw11, hw21, hw31) = (0.9, 0.3, 0.4);
+    let (hw12, hw22, hw32) = (0.2, 0.8, 0.2);
+    let (hw13, hw23, hw33) = (0.1, 0.5, 0.6);
+
+    // Layer 3: output weights
+    let (ow11, ow21, ow31) = (0.3, 0.7, 0.5);
+    let (ow12, ow22, ow32) = (0.6, 0.5, 0.2);
+    let (ow13, ow23, ow33) = (0.8, 0.1, 0.9);
 
     let inputs = vec![i1, i2, i3];
-    let weights = vec![vec![w11, w21, w31], vec![w12, w22, w32], vec![w13, w23, w33]];
+    let h_weights = vec![vec![hw11, hw21, hw31], vec![hw12, hw22, hw32], vec![hw13, hw23, hw33]];
+    let o_weights = vec![vec![ow11, ow21, ow31], vec![ow12, ow22, ow32], vec![ow13, ow23, ow33]];
 
-    let output = forward(inputs, weights);
+    let mut output;
+    output = forward(inputs, h_weights);
+    println!("Output layer 2: {:?}", output);
 
-    println!("Output: {:?}", output);
+    output = forward(output, o_weights);
+    println!("Output layer 3: {:?}", output);
 }
 
 fn forward(inputs:Vec<f64>, weights:Vec<Vec<f64>>) -> Vec<f64>{
